997a029ba324c365144d1f10a7f2be79ef080a19
Pradeep Fernando <pradeepfn@gmail.com>
2015-11-20 15:26:59 -0800

    CLI

--- src/topology.py
+++ src/topology.py
@@ -1,47 +1,51 @@
+import logging
+
 from mininet.topo import Topo
 from mininet.net import Mininet
 from mininet.util import dumpNodeConnections
 from mininet.log import setLogLevel
+from mininet.cli import CLI
 
 
+LOG = logging.getLogger(__name__);
 
 class ExpTopo(Topo):
-	#override the super class abstract method
-	def build(self,*args,**kwargs):
-		#create hosts
-		h1 = self.addHost('h1')
-		h2 = self.addHost('h2')
-		h3 = self.addHost('h3')
-		h4 = self.addHost('h4')
-		h5 = self.addHost('h5')
-		h6 = self.addHost('h6')
-		#create switches
-		s1 = self.addSwitch('s1')
-		s2 = self.addSwitch('s2')
-		s3 = self.addSwitch('s3')
-		s4 = self.addSwitch('s4')
-		s5 = self.addSwitch('s5')
-		s6 = self.addSwitch('s6')
+    # override the super class abstract method
+    def build(self, *args, **kwargs):
+        # create hosts
+        h1 = self.addHost('h1')
+        h2 = self.addHost('h2')
+        h3 = self.addHost('h3')
+        h4 = self.addHost('h4')
+        h5 = self.addHost('h5')
+        h6 = self.addHost('h6')
+        # create switches
+        s1 = self.addSwitch('s1')
+        s2 = self.addSwitch('s2')
+        s3 = self.addSwitch('s3')
+        s4 = self.addSwitch('s4')
+        s5 = self.addSwitch('s5')
+        s6 = self.addSwitch('s6')
 
-		#add links
-		self.addLink(s1,h1)	
-		self.addLink(s2,h2)	
-		self.addLink(s3,h3)	
-		self.addLink(s4,h4)	
-		self.addLink(s5,h5)	
-		self.addLink(s6,h6)	
-		
-		self.addLink(s1,s2)
-		self.addLink(s2,s3)
-		self.addLink(s3,s4)
-		self.addLink(s4,s5)
-		self.addLink(s5,s6)
-		
+        # add links
+        self.addLink(s1, h1)
+        self.addLink(s2, h2)
+        self.addLink(s3, h3)
+        self.addLink(s4, h4)
+        self.addLink(s5, h5)
+        self.addLink(s6, h6)
 
+        self.addLink(s1, s2)
+        self.addLink(s2, s3)
+        self.addLink(s3, s4)
+        self.addLink(s4, s5)
+        self.addLink(s5, s6)
 
 
 def printTopology():
-        #Create and test a simple network
+    net = None
+    try:
+        # Create and test a simple network
         topo = ExpTopo()
         net = Mininet(topo)
         net.start()
@@ -49,9 +53,16 @@ def printTopology():
         dumpNodeConnections(net.hosts)
         print "Testing network connectivity"
         net.pingAll()
+        CLI(net)
+        print "cleaning up.."
         net.stop()
+
+    except Exception as e:
+        LOG.error(e.message)
+        net.cleanup
+
 
 
 if __name__ == '__main__':
-	setLogLevel('info')
-	printTopology()	
+    setLogLevel('info')
+    printTopology()
