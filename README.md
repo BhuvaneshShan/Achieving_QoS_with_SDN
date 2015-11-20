aa135b78c69dd0c9b20522df0983af8fb93dffcc
Pradeep Fernando <pradeepfn@gmail.com>
2015-11-20 14:11:44 -0800

    topology creation

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Achieving_QoS_with_SDN
 computer networks project - software defined networking
+
+
+topology.py creates the mininet topoloy similar to the paper. However i did not connect the switch 1 and switch 6 so that
+it becomes a ring. Right now its a six switch six host linear topology
+
+
+## Test topology
+
+
+sudo python topology.py
---
+++ src/topology.py
@@ -0,0 +1,57 @@
+from mininet.topo import Topo
+from mininet.net import Mininet
+from mininet.util import dumpNodeConnections
+from mininet.log import setLogLevel
+
+
+
+class ExpTopo(Topo):
+	#override the super class abstract method
+	def build(self,*args,**kwargs):
+		#create hosts
+		h1 = self.addHost('h1')
+		h2 = self.addHost('h2')
+		h3 = self.addHost('h3')
+		h4 = self.addHost('h4')
+		h5 = self.addHost('h5')
+		h6 = self.addHost('h6')
+		#create switches
+		s1 = self.addSwitch('s1')
+		s2 = self.addSwitch('s2')
+		s3 = self.addSwitch('s3')
+		s4 = self.addSwitch('s4')
+		s5 = self.addSwitch('s5')
+		s6 = self.addSwitch('s6')
+
+		#add links
+		self.addLink(s1,h1)	
+		self.addLink(s2,h2)	
+		self.addLink(s3,h3)	
+		self.addLink(s4,h4)	
+		self.addLink(s5,h5)	
+		self.addLink(s6,h6)	
+		
+		self.addLink(s1,s2)
+		self.addLink(s2,s3)
+		self.addLink(s3,s4)
+		self.addLink(s4,s5)
+		self.addLink(s5,s6)
+		
+
+
+
+def printTopology():
+        #Create and test a simple network
+        topo = ExpTopo()
+        net = Mininet(topo)
+        net.start()
+        print "Dumping host connections"
+        dumpNodeConnections(net.hosts)
+        print "Testing network connectivity"
+        net.pingAll()
+        net.stop()
+
+
+if __name__ == '__main__':
+	setLogLevel('info')
+	printTopology()	
